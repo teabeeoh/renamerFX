2cf5670c0daabaecea02bf634017701d335b7460
Thomas Bolz <thomas.bolz@gmail.com>
2013-12-23 00:37:47 +0100

    commons-logging added

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("renamer.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("RenamerFX");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
