c3737eeec13b53f5e2081918e67ed903019fce6b
Thomas Bolz <thomas.bolz@gmail.com>
2013-12-26 11:40:15 +0100

    Fenstergröße vergrößert

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("renamer.fxml"));
         primaryStage.setTitle("RenamerFX");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root, 1000, 800));
         primaryStage.show();
     }
 
