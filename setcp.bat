df33b494c49c7ec7ccf6eead0adfb34e7a68a232
Thomas Bolz <mail@thomasbolz.de>
2014-01-09 22:15:50 +0100

    adding WiX and InnoSetup to the PATH (neede for native packaging with gradle javafx plugin)

--- setcp.bat
+++ setcp.bat
@@ -1,1 +1,8 @@
-set JAVA_HOME="c:\Program Files\Java\jdk1.7.0_45"
+set JAVA_HOME="C:\Program Files\Java\jdk1.8.0"
+REM adding JAVA executables
+set PATH=%JAVA_HOME%\bin;%PATH%
+REM adding InnoSetup
+set PATH=C:\Program Files\Inno Setup 5;%PATH%
+REM adding WiX
+set PATH=C:\Program Files\Windows Installer XML v3\bin;%PATH%
+
