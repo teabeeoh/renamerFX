b5de97419b326975bde423fa1074f84b23cf2f5c
Thomas Bolz <mail@thomasbolz.de>
2014-01-26 21:59:26 +0100

    512 pixel bild entfernt

--- assets/createIcons.sh
+++ assets/createIcons.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 alias inkscape='/Applications/Inkscape.app/Contents/Resources/bin/inkscape'
-for x in 16 32 64 128 256 512 ; do inkscape --export-area-drawing --export-png icon-${x}.png -w ${x} logo.svg ; done
+for x in 16 32 64 128 256 ; do inkscape --export-area-drawing --export-png icon-${x}.png -w ${x} logo.svg ; done
 mv *.png ../src/main/resources
-for x in 16 32 64 128 256 512 ; do inkscape --export-area-drawing --export-png shortcut-${x}.png -w ${x} logo.svg ; done
+for x in 16 32 64 128 256 ; do inkscape --export-area-drawing --export-png shortcut-${x}.png -w ${x} logo.svg ; done
 declare -i w
-for x in 16 32 64 128 256 512;
+for x in 16 32 64 128 256;
 do
     w=$x*2
 #    x=$x*2
