43b6ce5ec313fecad67e19a066f308479e19e6d9
Thomas Bolz <mail@thomasbolz.de>
2014-01-28 20:22:55 +0100

    neue Abmessungen

--- assets/createIcons.cmd
+++ assets/createIcons.cmd
@@ -5,7 +5,7 @@ FOR %%f IN (16 32 64 128 256) DO (
     inkscape --export-area-drawing --export-png icon-%%f.png -w %%f shortcut.svg
     inkscape --export-area-drawing --export-png shortcut-%%f.png -w %%f shortcut.svg
 )
-inkscape --export-area-drawing --export-png setup.png -w 48 shortcut.svg
+inkscape --export-area-drawing --export-png setup.png -w 55 -h 58 setup.svg
 move icon*.png ..\src\main\resources
 move shortcut*.png ..\src\deploy\package
 move setup.png ..\src\deploy\package
