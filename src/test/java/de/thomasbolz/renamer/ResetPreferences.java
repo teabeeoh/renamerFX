aee9452dbbd471bfc21fe4baf2c8713b1333e53e
Thomas Bolz <mail@thomasbolz.de>
2014-02-05 23:59:40 +0100

    Helper for resetting the prefs

---
+++ src/test/java/de/thomasbolz/renamer/ResetPreferences.java
@@ -0,0 +1,11 @@
+package de.thomasbolz.renamer;
+
+/**
+ * Created by thomas on 05.02.14.
+ */
+public class ResetPreferences {
+    public static void main(String[] args) {
+        RenamerGUI gui = new RenamerGUI();
+        gui.resetPreferences();
+    }
+}
